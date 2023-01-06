7375c3018a381de9e22d3d2416d88fa7a4fe7b40
Daniel Ramos <ghost2501dev@gmail.com>
2023-01-06 20:54:05 -0300

    Add fixtures

--- khanto/settings.py
+++ khanto/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FIXTURES_DIR = [
+    os.path.join(BASE_DIR, 'fixtures'),
+]
